ed828a8e5d51b7e55432f08586cdaf296b93338f
Vibha Masti <vsmasti@gmail.com>
2021-03-24 18:38:24 +0000

    :art: Moved main running code to run function

--- simpleMap/trafficlights.py
+++ simpleMap/trafficlights.py
@@ -15,11 +15,7 @@ import traci  # noqa
 sumoBinary = checkBinary('sumo-gui')
 sumoCmd = [sumoBinary, '-c', 'simpleMap.sumocfg']
 
-step = 0
-greenDur = 30
-yellowDur = 3
-cycle = 0
-traci.start(sumoCmd)
+
 
 # print(traci.getIDList())
 # print(dir(traci.inductionloop))
@@ -52,71 +48,83 @@ class SensorData:
         return self.throughput
 
 
-e3Detector_0 = SensorData('e3Detector_0')
-e3Detector_1 = SensorData('e3Detector_1')
-e3Detector_2 = SensorData('e3Detector_2')
-e3Detector_3 = SensorData('e3Detector_3')
+def run():
+    step = 0
+    greenDur = 30
+    yellowDur = 3
+    cycle = 0
+
+    e3Detector_0 = SensorData('e3Detector_0')
+    e3Detector_1 = SensorData('e3Detector_1')
+    e3Detector_2 = SensorData('e3Detector_2')
+    e3Detector_3 = SensorData('e3Detector_3')
 
 
-while step < 600:
-    traci.simulationStep()
+    while step < 600:
+        traci.simulationStep()
 
-    # The four variables store sensor data and update 
-    # every step, reset every cycle
-    
-    e3Detector_0.update()
-    e3Detector_1.update()
-    e3Detector_2.update()
-    e3Detector_3.update()
-
-    #The below section of code can be commented if traffic light control to be done by states already defines in netedit
-    #If below section left uncommented the traffic signal control done through TraCi
-    #Alternatively, all the setRedYellowGreenState calls could be replaced by setPhase("t0", index) where index is 0,1,2,3,4 to produce same functionality
-    if cycle in range(greenDur):
-        traci.trafficlight.setRedYellowGreenState("t0", "rrrGGgrrrrrr")
-        traci.trafficlight.setPhaseDuration("t0", greenDur)
-    elif cycle in range(greenDur+yellowDur):
-        traci.trafficlight.setRedYellowGreenState("t0", "rrryyyrrrrrr")
-        traci.trafficlight.setPhaseDuration("t0", yellowDur)  
-    elif cycle in range(2*greenDur+yellowDur):
-        traci.trafficlight.setRedYellowGreenState("t0", "rrrrrrGGgrrr")
-        traci.trafficlight.setPhaseDuration("t0", greenDur)
-    elif cycle in range(2*greenDur+2*yellowDur):
-        traci.trafficlight.setRedYellowGreenState("t0", "rrrrrryyyrrr")
-        traci.trafficlight.setPhaseDuration("t0", yellowDur)  
-    elif cycle in range(3*greenDur+2*yellowDur):
-        traci.trafficlight.setRedYellowGreenState("t0", "rrrrrrrrrGGg")
-        traci.trafficlight.setPhaseDuration("t0", greenDur)
-    elif cycle in range(3*greenDur+3*yellowDur):
-        traci.trafficlight.setRedYellowGreenState("t0", "rrrrrrrrryyy")
-        traci.trafficlight.setPhaseDuration("t0", yellowDur) 
-    elif cycle in range(4*greenDur+3*yellowDur):
-        traci.trafficlight.setRedYellowGreenState("t0", "GGgrrrrrrrrr")
-        traci.trafficlight.setPhaseDuration("t0", greenDur)
-    elif cycle in range(4*greenDur+4*yellowDur):
-        traci.trafficlight.setRedYellowGreenState("t0", "yyyrrrrrrrrr")
-        traci.trafficlight.setPhaseDuration("t0", yellowDur) 
-    else:
-        # The print statements run after every cycle of RR (throughput)
-        print('Cycle:', cycle)
-        print(e3Detector_0)
-        print(e3Detector_1)
-        print(e3Detector_2)
-        print(e3Detector_3)
+        # The four variables store sensor data and update 
+        # every step, reset every cycle
         
-        print()
+        e3Detector_0.update()
+        e3Detector_1.update()
+        e3Detector_2.update()
+        e3Detector_3.update()
 
-        # Reset cycle and detector values
-        e3Detector_0.reset()
-        e3Detector_1.reset()
-        e3Detector_2.reset()
-        e3Detector_3.reset()
+        #The below section of code can be commented if traffic light control to be done by states already defines in netedit
+        #If below section left uncommented the traffic signal control done through TraCi
+        #Alternatively, all the setRedYellowGreenState calls could be replaced by setPhase("t0", index) where index is 0,1,2,3,4 to produce same functionality
+        if cycle in range(greenDur):
+            traci.trafficlight.setRedYellowGreenState("t0", "rrrGGgrrrrrr")
+            traci.trafficlight.setPhaseDuration("t0", greenDur)
+        elif cycle in range(greenDur+yellowDur):
+            traci.trafficlight.setRedYellowGreenState("t0", "rrryyyrrrrrr")
+            traci.trafficlight.setPhaseDuration("t0", yellowDur)  
+        elif cycle in range(2*greenDur+yellowDur):
+            traci.trafficlight.setRedYellowGreenState("t0", "rrrrrrGGgrrr")
+            traci.trafficlight.setPhaseDuration("t0", greenDur)
+        elif cycle in range(2*greenDur+2*yellowDur):
+            traci.trafficlight.setRedYellowGreenState("t0", "rrrrrryyyrrr")
+            traci.trafficlight.setPhaseDuration("t0", yellowDur)  
+        elif cycle in range(3*greenDur+2*yellowDur):
+            traci.trafficlight.setRedYellowGreenState("t0", "rrrrrrrrrGGg")
+            traci.trafficlight.setPhaseDuration("t0", greenDur)
+        elif cycle in range(3*greenDur+3*yellowDur):
+            traci.trafficlight.setRedYellowGreenState("t0", "rrrrrrrrryyy")
+            traci.trafficlight.setPhaseDuration("t0", yellowDur) 
+        elif cycle in range(4*greenDur+3*yellowDur):
+            traci.trafficlight.setRedYellowGreenState("t0", "GGgrrrrrrrrr")
+            traci.trafficlight.setPhaseDuration("t0", greenDur)
+        elif cycle in range(4*greenDur+4*yellowDur):
+            traci.trafficlight.setRedYellowGreenState("t0", "yyyrrrrrrrrr")
+            traci.trafficlight.setPhaseDuration("t0", yellowDur) 
+        else:
+            # The print statements run after every cycle of RR (throughput)
+            print('Round:', round)
+            print(e3Detector_0)
+            print(e3Detector_1)
+            print(e3Detector_2)
+            print(e3Detector_3)
+            
+            print()
 
-        cycle = 0
+            # Reset cycle and detector values
+            e3Detector_0.reset()
+            e3Detector_1.reset()
+            e3Detector_2.reset()
+            e3Detector_3.reset()
 
-    cycle += 1
-    #END OF SECTION FOR CONTROLLING TRAFFIC SIGNAL WITH TRACI
-    
-    step += 1
+            cycle = 0
 
-traci.close()
+        cycle += 1
+        #END OF SECTION FOR CONTROLLING TRAFFIC SIGNAL WITH TRACI
+        
+        step += 1
+
+    traci.close()
+
+
+if __name__ == '__main__':
+    traci.start(sumoCmd)
+    run()
+
