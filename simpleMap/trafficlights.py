73003fcc211d38456ad4b0e3f1e0fe466a9e883d
Vibha Masti <vsmasti@gmail.com>
2021-04-08 16:45:58 +0000

    :sparkles: Made routes same as RL

--- simpleMap/trafficlights.py
+++ simpleMap/trafficlights.py
@@ -1,6 +1,9 @@
 import os
 import sys
 
+from generate_routes import TrafficGenerator
+
+
 # we need to import python modules from the $SUMO_HOME/tools directory
 if 'SUMO_HOME' in os.environ:
     tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
@@ -12,9 +15,15 @@ from sumolib import checkBinary  # noqa
 import traci  # noqa
 
 
-
 sumoBinary = checkBinary('sumo-gui')
 sumoCmd = [sumoBinary, '-c', 'simpleMap.sumocfg']
+
+MAX_STEPS = 5400
+SEED = 10000
+NO_CARS = 1000
+
+TrafficGen = TrafficGenerator(MAX_STEPS, NO_CARS)
+TrafficGen.generate_routefile(seed=SEED)
 
 class TrafficAgent:
     """Defines individual traffic signal behaviour."""
@@ -52,15 +61,31 @@ class TrafficAgent:
     def getYellowSignalString(self):
         return self._yellowSignalString
 
+def collect_waiting_times(waiting_times):
+    """
+    Retrieve the waiting time of every car in the incoming roads
+    """
+    incoming_roads = ["E2T", "N2T", "W2T", "S2T"]
+    car_list = traci.vehicle.getIDList()
+    for car_id in car_list:
+        wait_time = traci.vehicle.getAccumulatedWaitingTime(car_id)
+        road_id = traci.vehicle.getRoadID(car_id)  # get the road id where the car is located
+        if road_id in incoming_roads:  # consider only the waiting times of cars in incoming roads
+            waiting_times[car_id] = wait_time
+        else:   # not in incoming road
+            if car_id in waiting_times: # a car that was tracked has cleared the intersection
+                del waiting_times[car_id] 
+    total_waiting_time = sum(waiting_times.values())
+    return total_waiting_time
 
 def run():
     greenDur = 30
     yellowDur = 3
 
-    lane0 = TrafficAgent('-gneE1_0', 'rrrGGgrrrrrr', 'rrryyyrrrrrr', greenDur, yellowDur)   # east to west
-    lane1 = TrafficAgent('-gneE3_0', 'rrrrrrGGgrrr', 'rrrrrryyyrrr', greenDur, yellowDur)   # north to south
-    lane2 = TrafficAgent('gneE0_0', 'rrrrrrrrrGGg', 'rrrrrrrrryyy', greenDur, yellowDur)    # west to east
-    lane3 = TrafficAgent('-gneE5_0', 'GGgrrrrrrrrr', 'yyyrrrrrrrrr', greenDur, yellowDur)   # south to north 
+    lane0 = TrafficAgent('E2T_0', 'rrrGGgrrrrrr', 'rrryyyrrrrrr', greenDur, yellowDur)   # east to west
+    lane1 = TrafficAgent('N2T_0', 'rrrrrrGGgrrr', 'rrrrrryyyrrr', greenDur, yellowDur)   # north to south
+    lane2 = TrafficAgent('W2T_0', 'rrrrrrrrrGGg', 'rrrrrrrrryyy', greenDur, yellowDur)    # west to east
+    lane3 = TrafficAgent('S2T_0', 'GGgrrrrrrrrr', 'yyyrrrrrrrrr', greenDur, yellowDur)   # south to north 
 
     lanes = [lane0, lane1, lane2, lane3]
     laneIndex = 0
@@ -69,17 +94,32 @@ def run():
 
     cycle = 0
 
-    currentCycle = 0
     changeState = True
     nextSignal = lane0
 
     signalDuration = 0
     deadline = 0
 
+    queue_lengths = []
+    waiting_times = {}
+    old_total_wait = 0
+    rewards = []
 
-
-    while step < 250:
+    while step < MAX_STEPS:
         traci.simulationStep()
+
+        halt_N = traci.edge.getLastStepHaltingNumber("N2T")
+        halt_S = traci.edge.getLastStepHaltingNumber("S2T")
+        halt_E = traci.edge.getLastStepHaltingNumber("E2T")
+        halt_W = traci.edge.getLastStepHaltingNumber("W2T")
+
+        queue_length = halt_N + halt_S + halt_E + halt_W
+        queue_lengths.append(queue_length)
+
+        current_total_wait = collect_waiting_times(waiting_times)
+        reward = 0.9*old_total_wait - current_total_wait
+
+        rewards.append(reward)
 
         if changeState:
             # Defines behaviour whenever signals have to be changed.
@@ -116,6 +156,9 @@ def run():
         #END OF SECTION FOR CONTROLLING TRAFFIIC SIGNAL WITH TRACI
 
         step += 1
+        old_total_wait = current_total_wait
+
+    # PLOT FILES
 
     traci.close()
 
