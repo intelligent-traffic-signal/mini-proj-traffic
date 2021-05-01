a732aa0d7ae4276539117327cc02b8134c439452
Hrishit Chaudhuri <hrishitchaudhuri@gmail.com>
2021-05-01 10:27:46 +0530

    add configs

---
+++ osm-sims/OSMSarakki_edited/2021-04-30-12-24-40/additional_generator.py
@@ -0,0 +1,7 @@
+with open("./additional.xml", "w") as routes:
+    print("""<additional>""", file=routes)
+
+    for inc in range(3000):
+        print('\t<laneData id="int_%i" begin="%i" end="%i" excludeEmpty="true" file="./trafficinfo_detailed.xml" />' % (inc, inc, inc + 1), file=routes)
+
+    print("""</additional>""", file=routes)
